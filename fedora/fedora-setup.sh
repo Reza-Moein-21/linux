22b3d2fa80f31ba9aabf4fc48210c68c87dcc563
reza_moein <rezamoeinpe@gmail.com>
2022-10-20 12:42:36 +0330

    rearrange and comment some app's

--- fedora/fedora-setup.sh
+++ fedora/fedora-setup.sh
@@ -1,42 +1,48 @@
-sudo dnf update -y
-sudo dnf install -y audacity
-sudo dnf install -y gimp
-sudo dnf install -y persepolis
-sudo dnf install -y remmina
-sudo dnf install -y geary
-sudo dnf install -y vlc
-sudo dnf install -y gnome-tweaks
+dnf update -y
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf update -y
+dnf install -y audacity
+dnf install -y gimp
+dnf install -y persepolis
+dnf install -y remmina
+dnf install -y geary
+dnf install -y vlc
+dnf install -y gnome-tweaks
+dnf install -y nodejs
+dnf install -y openfortivpn
+dnf install -y rsms-inter-fonts-3.19-3.fc36.noarch
+dnf install -y vim
+dnf install -y unrar
+#dnf install -y smplayer smplayer-themes
+dnf install -y akmod-nvidia
+dnf install -y xorg-x11-drv-nvidia-cuda
 # setup and install skype
-sudo curl -o /etc/yum.repos.d/skype-stable.repo https://repo.skype.com/rpm/stable/skype-stable.repo
-sudo dnf install -y skypeforlinux
+#curl -o /etc/yum.repos.d/skype-stable.repo https://repo.skype.com/rpm/stable/skype-stable.repo
+#dnf install -y skypeforlinux
 
-sudo dnf install -y nodejs
+
 # setup and install vscode
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 dnf check-update
-sudo dnf install -y code
+dnf install -y code
 
 # setup and install docker 
-sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager \
-    --add-repo \
-    https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+#dnf -y install dnf-plugins-core
+#dnf config-manager \
+#   --add-repo \
+#    https://download.docker.com/linux/fedora/docker-ce.repo
+# dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
 
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
-sudo systemctl start docker
+#systemctl enable docker.service
+#systemctl enable containerd.service
+#systemctl start docker
 
-sudo dnf install -y openfortivpn
+
 
 # setup and install SDKMan
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 sdk version
-sudo dnf install -y rsms-inter-fonts-3.19-3.fc36.noarch
-sudo dnf install -y vim
-sudo dnf install -y unrar
-sudo dnf install -y smplayer smplayer-themes
-sudo dnf install -y akmod-nvidia
-sudo dnf install -y xorg-x11-drv-nvidia-cuda
+
