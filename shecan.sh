92e93400f4d13de50758bd309521edd9e2ab97f9
reza_moein <rezamoeinpe@gmail.com>
2025-10-10 16:01:00 +0330

    hyprland config added. shecan.sh updated

--- shecan.sh
+++ shecan.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-echo "nameserver 178.22.122.100" >> /etc/resolv.conf
-echo "nameserver 185.51.200.2" >> /etc/resolv.conf
+echo -e "nameserver 178.22.122.100\nnameserver 185.51.200.2" | sudo tee /etc/resolv.conf
